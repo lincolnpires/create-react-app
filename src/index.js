391e8a47895dc192c24f0910413ced53d765aeb2
Lincoln Pires <me@lincolnpires.com>
2019-03-11 22:26:51 +0100

    Add React and finishes play by play :)
    Do we need create-react-app?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('I have no idea')
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+ReactDOM.render(<App />, document.getElementById("app"));
